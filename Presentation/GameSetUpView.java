0a7be6c001438f7c48428cdb4a2d59e98416b8e9
fekorte <fe.korte@web.de>
2023-04-03 23:21:17 +0200

    Update GameSetUpView.java
    I'd really like to buy this book, just had a short glimpse in it in a bookstore. Two things stuck with me - 5 lines per method and always start with if (if it exists). Now, I would really like to know what's written in there about class attributes for example because now I created a lot. Should I have rather passed them in the parameter? Also I need to change the actionListener I think. It seems really impossible to have only five lines per method, maybe I even misunderstood because I didn't read the book that much. Would it not lead to way too many methods which would interrupt the flow of reading the code? Not sure if this here will lead to anything but I was just curious to try the 5 lines per method idea.

--- Presentation/GameSetUpView.java
+++ Presentation/GameSetUpView.java
@@ -12,85 +12,137 @@ import java.util.Iterator;
 
 public class GameSetUpView extends JFrame{
 
+    private final IPlayerManager playerManager;
+    private final IGameManager gameManager;
+    private final IWorldManager worldManager;
+    private final boolean standardRisk;
+    private JLabel playerInfo;
+    private JPanel setUpPanel;
+    private JButton addPlayerButton;
+    private JButton removePlayerButton;
+    private JButton startGameButton;
+    private JButton goBackButton;
+
     public GameSetUpView(IWorldManager worldManager, IPlayerManager playerManager, IGameManager gameManager, boolean standardRisk){
+
+        this.playerManager = playerManager;
+        this.gameManager = gameManager;
+        this.worldManager = worldManager;
+        this.standardRisk = standardRisk;
+
+        setUp();
+    }
+
+    private void setUp(){
+
+        this.playerInfo = new JLabel();
+        this.setUpPanel = new JPanel(new GridLayout(5, 1));
+        setUpPanel.add(playerInfo);
+
+        createButtons();
+        setUpAddPlayerButton();
+        deletePreviousGameState();
+        removePlayerButton();
+        startGameButton();
+
+        setFrameAttributes();
+    }
+
+    private void setFrameAttributes(){
+
+        this.add(setUpPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 400);
+        this.setLocationRelativeTo(null);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    private void deletePreviousGameState(){
 
         try {
             gameManager.newGame();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        JPanel setUpPanel = new JPanel(new GridLayout(5, 1));
+    private void createButtons(){
 
-        JLabel playerInfo = new JLabel();
-        setUpPanel.add(playerInfo);
+        addPlayerButton = new JButton("Add player");
+        removePlayerButton = new JButton("Remove player");
+        startGameButton = new JButton("Start game");
+        goBackButton = new JButton("Go back");
 
-        JButton addPlayerButton = new JButton("Add player");
-        JButton removePlayerButton = new JButton("Remove player");
-        JButton startGameButton = new JButton("Start game");
-        JButton goBackButton = new JButton("Go back");
+        addButtons();
+    }
+
+    private void addButtons(){
+
         setUpPanel.add(addPlayerButton);
         setUpPanel.add(removePlayerButton);
         setUpPanel.add(startGameButton);
         setUpPanel.add(goBackButton);
+    }
 
-        this.add(setUpPanel);
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
-        this.setSize(500, 400);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-
+    private void setUpAddPlayerButton(){
 
         addPlayerButton.addActionListener(a -> {
-            JFrame frame = new JFrame("New player");
-            JPanel panel = new JPanel(new GridLayout(0, 1));
+            JFrame addPlayerFrame = new JFrame("New player");
+            JPanel addPlayerPanel = new JPanel(new GridLayout(0, 1));
             JLabel nameLabel = new JLabel("Name: ");
             JTextField nameField = new JTextField();
-            panel.add(nameLabel);
-            panel.add(nameField);
+            addPlayerPanel.add(nameLabel);
+            addPlayerPanel.add(nameField);
 
             ButtonGroup group = new ButtonGroup();
             for (String color : playerManager.getAllowedColors()) {
                 JRadioButton button = new JRadioButton(color);
                 group.add(button);
-                panel.add(button);
+                addPlayerPanel.add(button);
             }
 
-            JButton doneButton = new JButton("Done");
 
-            panel.add(doneButton);
-            frame.add(panel);
-            frame.pack();
-            frame.setSize(400, 300);
-            frame.setLocationRelativeTo(null);
-            frame.setVisible(true);
+            addPlayerDoneButton(addPlayerFrame, addPlayerPanel, nameField, group);
 
-
-            doneButton.addActionListener(e -> {
-                String playerName = nameField.getText();
-                String playerColor = "";
-                for (Iterator<AbstractButton> buttons = group.getElements().asIterator(); buttons.hasNext();) {
-                    AbstractButton button = buttons.next();
-                    if (button.isSelected()) {
-                        playerColor = button.getText();
-                        break;
-                    }
-                }
-                try{
-                    playerManager.addPlayer(playerName, playerColor);
-                    playerInfo.setText(playerManager.getPlayersInfo());
-                    JOptionPane.showMessageDialog(null, playerName + " has been added.", "Player added", JOptionPane.INFORMATION_MESSAGE);
-
-                } catch (ExceptionEmptyInput | ExceptionPlayerAlreadyExists | ExceptionTooManyPlayers |
-                         ExceptionColorAlreadyExists c){
-                    JOptionPane.showMessageDialog(null, c.getMessage(), "Error", JOptionPane.INFORMATION_MESSAGE);
-
-                }
-                frame.dispose();
-            });
+            addPlayerFrame.dispose();
         });
+    }
+
+    private void addPlayerDoneButton(JFrame addPlayerFrame, JPanel addPlayerPanel, JTextField nameField, ButtonGroup group){
+
+        JButton doneButton = new JButton("Done");
+        addPlayerPanel.add(doneButton);
+        addPlayerFrame.add(addPlayerPanel);
+        addPlayerFrame.pack();
+        addPlayerFrame.setSize(400, 300);
+        addPlayerFrame.setLocationRelativeTo(null);
+        addPlayerFrame.setVisible(true);
+
+        doneButton.addActionListener(e -> {
+            String playerName = nameField.getText();
+            String playerColor = "";
+            for (Iterator<AbstractButton> buttons = group.getElements().asIterator(); buttons.hasNext();) {
+                AbstractButton button = buttons.next();
+                if (button.isSelected()) {
+                    playerColor = button.getText();
+                    break;
+                }
+            }
+            try{
+                playerManager.addPlayer(playerName, playerColor);
+                playerInfo.setText(playerManager.getPlayersInfo());
+                JOptionPane.showMessageDialog(null, playerName + " has been added.", "Player added", JOptionPane.INFORMATION_MESSAGE);
+
+            } catch (ExceptionEmptyInput | ExceptionPlayerAlreadyExists | ExceptionTooManyPlayers |
+                     ExceptionColorAlreadyExists c){
+                JOptionPane.showMessageDialog(null, c.getMessage(), "Error", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
+
+
+    private void removePlayerButton(){
 
         removePlayerButton.addActionListener(a -> {
 
@@ -103,6 +155,10 @@ public class GameSetUpView extends JFrame{
                 JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.INFORMATION_MESSAGE);
             }
         });
+    }
+
+
+    private void startGameButton(){
 
         startGameButton.addActionListener(a -> {
 
@@ -116,6 +172,10 @@ public class GameSetUpView extends JFrame{
                 throw new RuntimeException(e);
             }
         });
+    }
+
+
+    private void goBackButton(){
 
         goBackButton.addActionListener(a -> {
 
